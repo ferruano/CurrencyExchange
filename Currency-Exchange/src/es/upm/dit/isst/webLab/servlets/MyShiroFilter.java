5bad86dfd59488e2dd21e8e71e6b2d9845957747
Rodrigo Martín Martín <Rodri@MacBook-Pro-de-Rodrigo.local>
2019-04-09 12:55:19 +0200

    Ahora si

--- Currency-Exchange/src/es/upm/dit/isst/webLab/servlets/MyShiroFilter.java
+++ Currency-Exchange/src/es/upm/dit/isst/webLab/servlets/MyShiroFilter.java
@@ -7,5 +7,4 @@ import org.apache.shiro.web.servlet.ShiroFilter;
 
 @WebFilter(initParams = { @WebInitParam(name = "configPath", value = "classpath:shiro.ini") }, urlPatterns = {"/*"})
 public class MyShiroFilter extends ShiroFilter {
-	
 }
