72ec063c538dc69f1ba902e0a0c966d6636308af
Rodri <Rodri@10.151.219.55>
2019-04-10 19:32:27 +0200

    Campos vacíos y a 0 corregidos

--- Currency-Exchange/src/es/upm/dit/isst/webLab/servlets/CreateTransactionServlet.java
+++ Currency-Exchange/src/es/upm/dit/isst/webLab/servlets/CreateTransactionServlet.java
@@ -34,6 +34,7 @@ public class CreateTransactionServlet extends HttpServlet{
 		String email = req.getParameter("email");
 		String amount = req.getParameter("amount");
 		
+		
 		if (Double.parseDouble(amount) == 0.0) {
 			req.getSession().setAttribute( "correcto", true);
 			getServletContext().getRequestDispatcher( "/ManageView.jsp" ).forward( req, resp );
