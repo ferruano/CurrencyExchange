0a2d39f784605cda90fb98940efb5fb6e98aab02
narrietal <narrietal@bodhi-nico>
2019-04-29 15:09:28 +0200

    apicall finished v1

---
+++ Currency-Exchange/src/es/upm/dit/isst/webLab/servlets/ExchangeRatesServlet.java
@@ -0,0 +1,128 @@
+package es.upm.dit.isst.webLab.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.upm.dit.isst.webLab.dao.ClientDAO;
+import es.upm.dit.isst.webLab.dao.ClientDAOImplementation;
+import javax.ws.rs.GET;
+import javax.ws.rs.client.*;
+
+import org.json.JSONObject;
+import Constants.Constants;
+
+@WebServlet("/ExchangeRatesServlet")
+public class ExchangeRatesServlet extends HttpServlet{
+	
+	//Método local para pasar del numero que dentifica cada moneda a su acrónimo.
+	private String numberToCurrency(int number) {		
+		switch(number) {
+			case 1:
+				return "USD";
+			case 2:
+				return "EUR";
+			case 3:
+				return "JPY";
+			case 4:
+				return "GBP";
+			case 5:
+				return "CHF";
+			case 6:
+				return "AUD";
+			case 7:
+				return "CAD";
+			default:
+				return "EUR";
+		}	
+	}
+	
+	//Método para no mostrar la fila que coincida con la moneda escogida (para evitar redundancia)
+	public String[] getRowHidden(String originCurrency){
+		String[] rowsHidden = new String[7];
+		
+		for(int i=0; i<Constants.currency.length;i++){
+			if(originCurrency.equals(Constants.currency[i])){
+				rowsHidden[i]="display:none;visibility:hidden;";
+			}else{
+				rowsHidden[i]="";
+			}
+		}
+		return rowsHidden;
+	}
+	
+	//Método que llama a la API e introduce el valor de cada moneda en el array exRates
+	@GET 
+	public double[] getExRate(String originCurrency){
+		String exchangeKey = "58ffdde58954130b3ede";
+		String exchangeKey2 = "d49444542eb8ba4223ea"; //Clave por si usamos la api más de 100 veces cada hora
+		
+		Client client = ClientBuilder.newClient();
+		double[] exRates = new double[7];
+		
+		for(int i=0; i<Constants.currency.length;i++) {
+			String infoExchange= client.target("https://free.currencyconverterapi.com/api/v6/convert")
+					.queryParam("q",originCurrency+"_"+Constants.currency[i])
+					.queryParam("compact", "ultra")
+					.queryParam("apiKey", exchangeKey2)
+					.request()
+			        .get(String.class);
+			
+			JSONObject infoEx = new JSONObject(infoExchange);
+			double exRate = infoEx.getDouble(originCurrency+"_"+Constants.currency[i]);
+			exRates[i] = exRate;
+		}
+		
+		client.close();
+		return exRates;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		ClientDAO cdao = ClientDAOImplementation.getInstance();
+		String email = req.getParameter("email");
+		req.getSession().setAttribute( "client", cdao.read(email) );
+		
+		String OriginCurrency = req.getParameter("orCurrency"); //Moneda que escoges como origen en el jsp
+		double[] exRates = new double[7];
+		String[] rowsHidden = new String[7];
+		
+		//IF para que la primera vez que se entre en la página el valor de la moneda origen sea el que haya
+		//escogido el usuario como moneda local. Las deḿas veces será el escogido en la lista del jsp.
+		if(OriginCurrency != null) {
+			req.getSession().setAttribute( "orCurrency", OriginCurrency);
+			exRates = getExRate(OriginCurrency);
+			rowsHidden = getRowHidden(OriginCurrency);
+		}else{
+			String userCurr = numberToCurrency(cdao.read(email).getLocalCurrency());
+			req.getSession().setAttribute( "orCurrency", userCurr);
+			exRates = getExRate(userCurr);
+			rowsHidden = getRowHidden(userCurr);
+		}
+		
+		req.getSession().setAttribute( "usdRate", exRates[0]);
+		req.getSession().setAttribute( "eurRate", exRates[1]);
+		req.getSession().setAttribute( "jpyRate", exRates[2]);
+		req.getSession().setAttribute( "gbpRate", exRates[3]);
+		req.getSession().setAttribute( "chfRate", exRates[4]);
+		req.getSession().setAttribute( "audRate", exRates[5]);
+		req.getSession().setAttribute( "cadRate", exRates[6]);
+		
+		req.getSession().setAttribute( "usdHide", rowsHidden[0]);
+		req.getSession().setAttribute( "eurHide", rowsHidden[1]);
+		req.getSession().setAttribute( "jpyHide", rowsHidden[2]);
+		req.getSession().setAttribute( "gbpHide", rowsHidden[3]);
+		req.getSession().setAttribute( "chfHide", rowsHidden[4]);
+		req.getSession().setAttribute( "audHide", rowsHidden[5]);
+		req.getSession().setAttribute( "cadHide", rowsHidden[6]);
+
+		getServletContext().getRequestDispatcher( "/ExchangeRates.jsp" ).forward( req, resp );
+	}
+	
+	
+}
